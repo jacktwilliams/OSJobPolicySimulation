3b55480899093f254f2d87707cdccf87e267ecee
Jack Williams <jwilliams17@winona.edu>
2019-09-30 16:41:03 -0500

    Fixed many issues with the utilities to alter jobs in memory. Fixed my policies to have a working simple run, buffered run, and spooling run.

--- Williams_HW1.lisp
+++ Williams_HW1.lisp
@@ -11,13 +11,13 @@
 (defvar *dma*)
 
 (defstruct job name bursts inputs)
+(defstruct input size)
 (defvar *jobs* (list))
 (defvar *job-size* 200)
-(defvar *inp-size* 100)
 
 (defstruct policy conditions actions)
 (defstruct transfer transfer-params obj start-time)
-(defstruct interrupt time result)
+(defstruct interrupt time result type)
 (defstruct context storage-jobs-table transfers interrupts time)
 
 (defparameter *context* nil
@@ -30,10 +30,14 @@
   (format out "~a: ~a~%" time str))
 
 (defun job-needs-input (job)
+  "States whether job needs input in memory for next burst and is lacking that input currently."
   (let ((bursts (job-bursts job))
         (inputs (job-inputs job)))
-    (and (not (= 0 (length inputs)))
+    (and (null (job-in-location *memory-input*))
          (= (length bursts) (length inputs)))))
+
+(defun get-input-size (job)
+  (first (job-inputs job)))
 
 (defun take-input-from-job (job)
   "Shorten JOB's input list and return the altered job."
@@ -55,47 +59,77 @@
           ((and (equal n1 'disk) (equal n2 'dma)) *disk->dma*))))
 
 (defun get-time-for-transfer (params obj)
-  (let* ((size (if (job-p obj) *job-size* *inp-size*))
+  (let* ((size (if (job-p obj) *job-size* (input-size obj)))
          (itrs (ceiling (/ size (transfer-params-block params)))))
     (* itrs (transfer-params-speed params))))
 
 (defun remove-obj-from-location (loc &optional (obj nil))
   "Given LOCATION, if an OBJ is supplied, remove that object from location, otherwise remove the first object in location and return it."
-  (let ((objs-in-location (cdr (assoc loc (context-storage-jobs-table *context*)))))
+  (let ((objs-in-location (cdr (assoc loc (context-storage-jobs-table *context*))))
+        (removed-thing obj)
+        (remove-test (lambda (key item)
+                       (or (and (job-p key)
+                                (job-p item)
+                                (equal (job-name key) (job-name item)))
+                           (and (input-p key)
+                                (input-p item)
+                                (equal (input-size key) (input-size item)))))))
     (if (not (null obj))
-        (rplacd (assoc loc (context-storage-jobs-table *context*)) (remove obj objs-in-location))
-        (let ((removed-thing (first objs-in-location)))
-          (rplacd (assoc loc (context-storage-jobs-table *context*)) (remove removed-thing objs-in-location))
-          removed-thing))))
+        (progn
+          (setf removed-thing (find obj objs-in-location :test remove-test))
+          (rplacd (assoc loc (context-storage-jobs-table *context*)) (remove removed-thing objs-in-location)))
+        (progn
+          (setf removed-thing (first objs-in-location))
+          (rplacd (assoc loc (context-storage-jobs-table *context*)) (remove removed-thing objs-in-location))))
+    (let ((obj-size (if (job-p removed-thing) *job-size* (if (input-p removed-thing) (input-size removed-thing)
+                                                             0))))
+      (setf (holder-allocated loc) (- (holder-allocated loc) obj-size)))
+    removed-thing))
 
 (defun add-obj-to-location (loc obj)
   (let* ((s-j-t (context-storage-jobs-table *context*))
+         (obj-size (if (job-p obj) *job-size* (input-size obj)))
+         (remaining-loc-space (- (holder-size loc) (holder-allocated loc)))
+         (new-loc-allocated (+ (holder-allocated loc) obj-size))
          (loc-jobs (cdr (assoc loc s-j-t)))
          (new-loc-jobs (append loc-jobs (list obj))))
+    (when (> obj-size remaining-loc-space) (error "Not enough space for object."))
+    (setf (holder-allocated loc) new-loc-allocated)
     (rplacd (assoc loc s-j-t) new-loc-jobs)
     (setf (context-storage-jobs-table *context*) s-j-t)))
 
+(defun job-burst-needs-input (job)
+  (let ((inp-length (length (job-inputs job)))
+        (burst-length (length (job-bursts job))))
+    (= inp-length burst-length)))
+
 (defun consume-burst (job)
   "Pop the first item off the JOB's burst list, knowing the job is in memory."
-  (remove-obj-from-location *memory* job)
-  (when (not (null (job-in-location *memory*))) ;;sanity check
-    (error "Job should've been removed."))
-  (when (= 0 (length (job-bursts job)))
-    (error "No bursts left."))
-  (let ((new-job (take-burst-from-job job)))
-    (if (= 0 (length (job-bursts new-job)))
-        (timestamped-print (context-time *context*) (concatenate 'string (job-name job) " finished."))
-        (add-obj-to-location *memory* new-job))))
+  (let ((real-job (remove-obj-from-location *memory* job))) ;; replace job with actual job in memory
+    (when (and (not (null (job-in-location *memory*)))
+               (job-p (job-in-location *memory*))) ;;sanity check. TODO: if this fixes bug, take note that no input should be in *memory* it should be in *memory-input*
+      (error "Job should've been removed."))
+    (when (= 0 (length (job-bursts real-job)))
+      (error "No bursts left."))
+    (let ((new-job (take-burst-from-job real-job)))
+      (if (= 0 (length (job-bursts new-job)))
+          (timestamped-print (context-time *context*) (concatenate 'string (job-name job) " finished."))
+          (add-obj-to-location *memory* new-job))
+      new-job)))
 
 (defun consume-input (job)
   "Pop the first item off the JOB's input list list, knowing the job is in memory."
-  (remove-obj-from-location *memory* job)
-  (when (not (null (job-in-location *memory*))) ;;sanity check
-    (error "Job should've been removed."))
-  (when (= 0 (length (job-inputs job)))
-    (error "No inputs left."))
-  (let ((new-job (take-input-from-job job)))
-    (add-obj-to-location *memory* new-job)))
+  (let ((real-job (remove-obj-from-location *memory* job))) ;; replace job with actual job in memory if it hasn't already been taken and sent as arg
+    (when (not (null (job-in-location *memory*))) ;;sanity check
+      (error "Job should've been removed."))
+    (when (= 0 (length (job-inputs real-job)))
+      (error "No inputs left."))
+    (let ((inp (remove-obj-from-location *memory-input*)))
+      (when (equal  nil inp)
+        (error "No input was in memory.")))
+    (let ((new-job (take-input-from-job real-job)))
+      (add-obj-to-location *memory* new-job)
+      new-job)))
 
 
 (defun move-with-result (loc1 loc2 obj res)
@@ -118,7 +152,7 @@
                    (when (not (null res)) (funcall res)))))
     (timestamped-print time (with-output-to-string (outs) (format outs "Moving ~a from ~a to ~a" obj-name (holder-name loc1) (holder-name loc2))))
     (setf (context-transfers *context*) (append (context-transfers *context*) (list transfer)))
-    (setf (context-interrupts *context*) (append (context-interrupts *context*) (list (make-interrupt :time finish-time :result result))))))
+    (setf (context-interrupts *context*) (append (context-interrupts *context*) (list (make-interrupt :time finish-time :result result :type 'transfer))))))
 
 (defun job-in-location (location)
   "Return the first job that exists in LOCATION."
@@ -129,22 +163,34 @@
   "Return T if no jobs exist outside of tape."
   (dolist (location (map 'list #'car (context-storage-jobs-table *context*)))
     (when (and (not (equal 'tape (holder-name location)))
-               (job-in-location location))
+               (job-p (job-in-location location)))
       (return-from jobs-only-on-tape nil)))
   (return-from jobs-only-on-tape t))
 
+(defun job-is-running (job)
+  (dolist (interrupt (context-interrupts *context*) nil)
+    (when (equal (interrupt-type interrupt) (job-name job))
+      (return-from job-is-running t))))
+
+(defun put-inputs-on-tape (job)
+  (dolist (input (job-inputs job))
+    (add-obj-to-location *tape* (make-input :size input))))
+
 (defun run-job ()
   "Knowing a job is in memory, execute it. This entails proper logging and removing the cpu burst from the job's exec-sequence."
-  (let* ((job (remove-obj-from-location *memory*))
+  (let* ((job (job-in-location *memory*))
          (run-time (first (job-bursts job))))
     (when (job-needs-input job)
       (error "~a needs input" job))
     (timestamped-print (context-time *context*) (concatenate 'string "Start executing " (job-name job)))
     (setf *user-exec-time* (+ run-time *user-exec-time*))
     (setf (context-interrupts *context*) (append (context-interrupts *context*) (list (make-interrupt :time (+ run-time (context-time *context*))
+                                                                                                      :type (job-name job)
                                                                                                       :result (lambda ()
-                                                                                                                (timestamped-print (context-time *context*) (concatenate 'string "Done executing " (job-name job)))
-                                                                                                                (consume-burst job))))))))
+                                                                                                                (let ((job (job-in-location *memory*)))
+                                                                                                                  (timestamped-print (context-time *context*) (concatenate 'string "Done executing " (job-name job)))
+                                                                                                                  (when (job-burst-needs-input job) (consume-input job))
+                                                                                                                  (consume-burst job)))))))))
 
 (defun next-interrupt ()
   "Find interrupt with next lowest time. Execute the interrupt's result and set the context time accordingly."
@@ -203,7 +249,7 @@
                 (setf bursts (append bursts (list num)))))
           (setf *jobs* (append *jobs* (list (make-job :name name :inputs inputs :bursts bursts))))))))
 
-  (with-open-file (in-file (concatenate 'string dir "test1.dat"))
+  (with-open-file (in-file (concatenate 'string dir "test2.dat"))
     (setf *tape->disk* (make-transfer-params
                         :sender 'tape
                         :receiver 'disk
@@ -217,10 +263,10 @@
                           :speed (read in-file)))
     (let ((total-mem (read in-file))
           (mem-in (read in-file)))
-      (setf *memory* (make-holder :name 'memory :size (- total-mem mem-in) :allocated 0))
+      (setf *memory* (make-holder :name 'memory :size total-mem :allocated 0))
       (setf *memory-input* (make-holder :name 'mem-in :size mem-in :allocated 0)))
     (setf *dma* (make-holder :name 'dma :size (read in-file) :allocated 0))
     (read-jobs in-file)))
 
-;(load (concatenate 'string dir "testing.lisp"))
+(load (concatenate 'string dir "testing.lisp"))
 (load (concatenate 'string dir "run-strategies.lisp"))
--- run-strategies.lisp
+++ run-strategies.lisp
@@ -1,32 +1,126 @@
+(defun reset-state ()
+  (defun remove-all (holder)
+    (do ((obj (remove-obj-from-location holder) (remove-obj-from-location holder)))
+        ((null obj) t)))
+  (setf *user-exec-time* 0)
+  (dolist (holder (list *disk* *dma* *memory* *memory-input*))
+    (when (context-p *context*) ;; if context has been initialized clear everything off of each holder when resetting state
+      (remove-all holder))
+    (setf (holder-allocated holder) 0)))
+
+(defun no-transfers-happening-cond ()
+  (= 0 (length (context-transfers *context*))))
+
 (defun job-needs-input-cond ()
   (let ((job (job-in-location *memory*)))
     (and (not (null job))
          (job-needs-input job)
-         (every (lambda (transfer) (not (equal (transfer-obj transfer) "input"))) (context-transfers *context*)))))
+         (no-transfers-happening-cond))))
 
 (defun job-should-run-cond ()
   (let ((job (job-in-location *memory*)))
     (and (not (null job))
+         (not (job-is-running job))
          (not (job-needs-input job)))))
 
+(defun job-on-tape-cond ()
+  (let ((job (job-in-location *tape*)))
+    (and (not (null job))
+         (job-p job))))
+
+(defun move-job-from-tape-to-mem ()
+  (let ((job (cadr (assoc *tape* (context-storage-jobs-table *context*)))))
+    (move-with-result *tape* *disk* job
+                      (lambda ()
+                        (move-with-result *disk* *memory* job nil)))))
+
+(defun move-input-from-tape-to-mem ()
+  (let* ((job (job-in-location *memory*))
+        (input (make-input :size (get-input-size job))))
+    (move-with-result *tape* *disk* input
+                      (lambda ()
+                        (move-with-result *disk* *memory-input* input nil)))))
+
+(defun move-input-from-tape-to-disk ()
+  (let* ((job (job-in-location *memory*))
+         (input (make-input :size (get-input-size job))))
+    (move-with-result *tape* *disk* input nil)))
+
+(defun move-input-from-disk-to-mem ()
+  (let* ((job (job-in-location *memory*))
+         (input (input-ready-at-loc *disk* job)))
+    (when (null input)
+      (error "input wasn't ready"))
+    (move-with-result *disk* *memory-input* input nil)))
+
+(defun add-interrupt-to-check-for-action ()
+  "Add interrupts to check if there are additional things we can do concurrently."
+  (setf (context-interrupts *context*) (append (context-interrupts *context*)
+                                               (list (make-interrupt :time (context-time *context*) :result nil :type 'checker)))))
+
 (defun simple-run ()
-  (let* ((p-move-job (make-policy :conditions (list #'jobs-only-on-tape
-                                                    (lambda ()
-                                                      (let ((job (job-in-location *tape*)))
-                                                        (and (not (null job))
-                                                             (job-p job)))))
-                                  :actions (list (lambda ()
-                                                   (let ((job (cadr (assoc *tape* (context-storage-jobs-table *context*)))))
-                                                     (move-with-result *tape* *disk* job
-                                                                       (lambda ()
-                                                                         (move-with-result *disk* *memory* job nil))))))))
+  (reset-state)
+  (let* ((p-move-job (make-policy :conditions (list #'jobs-only-on-tape #'job-on-tape-cond)
+                                  :actions (list #'move-job-from-tape-to-mem)))
          (p-job-needs-input (make-policy :conditions (list #'job-needs-input-cond)
-                                         :actions (list (lambda ()
-                                                          (let ((job (job-in-location *memory*)))
-                                                            (move-with-result *tape* *disk* "input"
-                                                                              (lambda ()
-                                                                                (move-with-result *disk* *memory-input* "input" (lambda ()
-                                                                                                                                  (consume-input job))))))))))
+                                         :actions (list #'move-input-from-tape-to-mem)))
          (p-run-job (make-policy :conditions (list #'job-should-run-cond)
                                  :actions (list #'run-job))))
     (run *jobs* (list p-move-job p-job-needs-input p-run-job))))
+
+(defun ready-for-input-in-mem ()
+  (ready-for-input-at-loc *memory-input*))
+
+(defun ready-for-input-at-loc (loc)
+  (let ((job (job-in-location *memory*))
+        (remaining-inp-space (- (holder-size loc) (holder-allocated loc))))
+    (and (not (null job))
+         (<= (get-input-size job) remaining-inp-space)
+         (no-transfers-happening-cond)
+         (> (length (job-inputs job)) 0))))
+
+(defun input-ready-at-loc (loc job)
+  "Returns an input object if it's available, otherwise nil."
+  (let* ((input (make-input :size (get-input-size job)))
+         (loc-objs (cdr (assoc loc (context-storage-jobs-table *context*))))
+         (avail-inp (find input loc-objs :test (lambda (key item)
+                                                 (and (input-p item)
+                                                      (equal (input-size key) (input-size item)))))))
+    (if (not (null avail-inp))
+        avail-inp
+        nil)))
+
+(defun buffered-run ()
+  (reset-state)
+  (let* ((p-move-job (make-policy :conditions (list #'jobs-only-on-tape #'job-on-tape-cond)
+                                  :actions (list #'move-job-from-tape-to-mem)))
+         (p-run-job (make-policy :conditions (list #'job-should-run-cond)
+                                 :actions (list #'run-job #'add-interrupt-to-check-for-action))) ;;interrupt to check to see if we can start bringing in input
+         (p-bring-input (make-policy :conditions (list #'ready-for-input-in-mem)
+                                     :actions (list #'move-input-from-tape-to-mem #'add-interrupt-to-check-for-action)))) ;; interrupt to check to see if we can bring more input
+    (run *jobs* (list p-move-job p-run-job p-bring-input))))
+
+
+(defun buffered-spooled-run ()
+  (reset-state)
+  (let* ((p-move-job (make-policy :conditions (list #'jobs-only-on-tape #'job-on-tape-cond)
+                                  :actions (list (lambda ()
+                                                   (let ((job (job-in-location *tape*)))
+                                                     (put-inputs-on-tape job)))
+                                                 #'move-job-from-tape-to-mem)))
+         (p-run-job (make-policy :conditions (list #'job-should-run-cond)
+                                 :actions (list #'run-job #'add-interrupt-to-check-for-action))) ;;interrupt to check to see if we can start bringing in input
+         (p-bring-input-to-disk (make-policy :conditions (list (lambda ()
+                                                                 (let ((job (job-in-location *memory*)))
+                                                                   (and (not (null job))
+                                                                        (input-ready-at-loc *tape* job)
+                                                                        (ready-for-input-at-loc *disk*)))))
+                                             :actions (list #'move-input-from-tape-to-disk #'add-interrupt-to-check-for-action)))
+         (p-bring-input-to-mem (make-policy :conditions (list (lambda ()
+                                                                (let ((job (job-in-location *memory*)))
+                                                                  (and (not (null job))
+                                                                       (input-ready-at-loc *disk* job)
+                                                                       (ready-for-input-at-loc *memory-input*)))))
+                                            :actions (list #'move-input-from-disk-to-mem #'add-interrupt-to-check-for-action))) ;; interrupt to check to see if we can bring more input
+         )
+    (run *jobs* (list p-move-job p-run-job p-bring-input-to-mem p-bring-input-to-disk))))
